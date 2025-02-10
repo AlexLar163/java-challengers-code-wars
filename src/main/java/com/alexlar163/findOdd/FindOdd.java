4c9573698288ccb7066ae1bc485f069d603ce552
AlexLar163 <alexlar163@gmail.com>
2025-02-09 19:31:17 -0500

    Add FindOdd challenge

--- src/main/java/com/alexlar163/findOdd/FindOdd.java
+++ src/main/java/com/alexlar163/findOdd/FindOdd.java
@@ -1,4 +1,25 @@
-package com.alexlar163;
+package com.alexlar163.findOdd;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class FindOdd {
+    public static void main (String[] args) {
+//        System.out.println(findIt(new int[]{7}));
+//        System.out.println(findIt(new int[]{0}));
+//        System.out.println(findIt(new int[]{1,1,2}));
+//        System.out.println(findIt(new int[]{0,1,0,1,0}));
+        System.out.println(findIt(new int[]{1,2,2,3,3,3,4,3,3,3,2,2,1}));
+    }
+    public static int findIt(int[] a) {
+        int result = 0;
+        // basicamente copara el binario y saca el valor
+        //        1 ^ 0 = 1
+        //        1 ^ 1 = 0
+        //        0 ^ 1 = 1
+        for (int i : a) {
+            result ^= i;
+        }
+        return result;
+    }
 }
