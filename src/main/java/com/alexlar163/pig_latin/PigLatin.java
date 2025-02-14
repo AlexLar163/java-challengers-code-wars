b41d3ab872dbab7982c375b45e309866cae8879d
AlexLar163 <alexlar163@gmail.com>
2025-02-14 12:45:41 -0500

    Update PigLatin Challenge

--- src/main/java/com/alexlar163/pig_latin/PigLatin.java
+++ src/main/java/com/alexlar163/pig_latin/PigLatin.java
@@ -11,15 +11,18 @@ import java.util.Arrays;
 
 public class PigLatin {
     public static void main(String[] args) {
-        System.out.println(pigIt("Pig latin is cool"));
+//        System.out.println(pigIt("Pig latin is cool"));
+        System.out.println(pigIt("tora o mores !"));
     }
     public static String pigIt(String str) {
         String[] strList = str.split(" ");
         for (int i = 0; i < strList.length; i++){
             char firstChar = strList[i].charAt(0);
-            strList[i] = strList[i]
-                    .substring(1)
-                    .concat(firstChar + "ay");
+            if(strList[i].matches("[a-zA-Z]+")) {
+                strList[i] = strList[i]
+                        .substring(1)
+                        .concat(firstChar + "ay");
+            }
         }
         return String.join(" ", strList);
     }
